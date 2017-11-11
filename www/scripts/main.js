570f38a36899c990a9aa39f85d7dad7ca1009f4a
zhzhang2012 <zhzhangtony@gmail.com>
2017-11-11 07:10:39 -0800

    chang color

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -36,10 +36,10 @@ var loadWeather = function(cb,city){
     woeid: '',
     unit: 'f',
     success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html = '<div style="background-color: #000;width:100%;height: 100%;display: block;"><h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
       html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
       html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul></div>';
 
       $("#weather-container").html(html);
       cb();
