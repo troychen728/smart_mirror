0f11fe7c29a0c8261c0dc3b226ebbb7d81c658e4
Danny Wang <yulwang@g.hmc.edu>
2017-11-11 03:52:22 -0800

    Connected to Google Maps

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -41,6 +41,30 @@ var loadWeather = function(cb,city){
    
 }
 
+var loadMapDirections = function(cb,destination){
+  var mapContainer = document.getElementById("map-container");
+  while (mapContainer.hasChildNodes()) {
+    mapContainer.removeChild(mapContainer.lastChild);
+  }
+  var mapIFrame = document.createElement("IFRAME");
+  mapIFrame.setAttribute('width', '450');
+  mapIFrame.setAttribute('height', '250');
+  mapIFrame.setAttribute('frameborder', '0');
+  mapIFrame.setAttribute('style', 'border:0');
+  if (destination == "work") {
+    destination = "Bauer+Center";
+  } else if (destination == "school") {
+    destination = "Harvey+Mudd+College";
+  } else if (destination == "home") {
+    destination = "College+Park+Apartment+Homes"
+  } else if (destination == "gym") {
+    destination = "LA+Fitness"
+  }
+  mapIFrame.setAttribute('src', 'https://www.google.com/maps/embed/v1/directions?key=AIzaSyCrwiBAYIUr_b0bqB8RzYEhmF846TwC7Zg&origin=Edmunds+Ballroom&destination=' + destination);
+  mapContainer.appendChild(mapIFrame);
+  cb();
+}
+
 var loadFirework = function() {
   document.getElementById("firework-container").appendChild(fireworkCanvas);
   fireworkCanvas.width = SCREEN_WIDTH;
@@ -97,6 +121,10 @@ var toggleModal = function(currentInput) {
 
         case 'Stock':
           loadStock(cb);
+          break;
+
+        case 'trafficintent':
+          loadMapDirections(cb, currentInput.slots.Destination)
           break;
       }
     }
